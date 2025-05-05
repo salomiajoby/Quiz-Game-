841e43240115c2727e65d4b3928bfdbd4d425e5a
salomiajoby <160570055+salomiajoby@users.noreply.github.com>
2025-05-05 14:03:33 +1200

    completed base code

--- quiz_game.py
+++ quiz_game.py
@@ -1,12 +1,14 @@
 print("Welcome to my computer quiz game!")
 
-playing = input("Do you want to play? (yes/no) ") 
+playing = input("Do you want to play? (yes/no) ").lower() 
 
 if playing != "yes":
     quit()
     
 print("Okay! Let's play!")
 
-answer = input("What does CPU stand for? ")
+answer = input("What does CPU stand for? ").lower()
 if answer == "central processing unit":
     print("Correct!")
+else: 
+    print("Incorrect!")
