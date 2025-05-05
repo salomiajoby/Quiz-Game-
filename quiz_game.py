a92f2d5b04026bf258e48425bee2d15570c37b70
salomiajoby <160570055+salomiajoby@users.noreply.github.com>
2025-05-05 14:18:18 +1200

    completed the code

--- quiz_game.py
+++ quiz_game.py
@@ -1,14 +1,53 @@
-print("Welcome to my computer quiz game!")
+print("Welcome to my general knowledge quiz game!")
 
 playing = input("Do you want to play? (yes/no) ").lower() 
+print("You will be asked 5 questions. For each question, type your answer and press enter.")
 
 if playing != "yes":
     quit()
     
 print("Okay! Let's play!")
+score = 0 
 
-answer = input("What does CPU stand for? ").lower()
-if answer == "central processing unit":
+answer = input("What is the 4th letter of the Greek Alphabet? ").lower()
+if answer == "delta":
     print("Correct!")
+    score += 1
 else: 
     print("Incorrect!")
+
+
+answer = input("What is the smallest unit of matter? ").lower()
+if answer == "atom":
+    print("Correct!")
+    score += 1
+else: 
+    print("Incorrect!")
+    
+
+answer = input("What is acrophobia the fear of? ").lower()
+if answer == "heights":
+    print("Correct!")
+    score += 1
+else: 
+    print("Incorrect!")
+
+
+answer = input("What does RAM stand for? ").lower()
+if answer == "random access memory":
+    print("Correct!")
+    score += 1
+else: 
+    print("Incorrect!")
+
+answer = input("What is the scientific theory that explains the origin of the universe? ").lower()
+if answer == "the big bang theory":
+    print("Correct!")
+    score += 1
+else: 
+    print("Incorrect!")
+
+
+print("You got " + str(score) + " questions correct!!")
+print("That will be " + str((score / 5) * 100) + "%") 
+print("Thanks for playing!")
